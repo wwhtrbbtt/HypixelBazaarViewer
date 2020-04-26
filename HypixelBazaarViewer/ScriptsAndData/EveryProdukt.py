d981b893231f699614bc936e0c8355b7513a2b2b
wwhtrbbtt <64318024+wwhtrbbtt@users.noreply.github.com>
2020-04-26 16:15:12 +0200

    Added a feature, you now can see the best product

--- HypixelBazaarViewer/ScriptsAndData/EveryProdukt.py
+++ HypixelBazaarViewer/ScriptsAndData/EveryProdukt.py
@@ -1,10 +1,3 @@
-import requests
-import json
-import string
-import sys
-sys.path.insert(1, 'ScriptsAndData')
-
-
 #every Bazaar product: https://api.hypixel.net/skyblock/bazaar/products?key=YourApiKey
 
 #Thanks to ThisIsMyPassword for this spreadsheet
@@ -13,6 +6,20 @@ sys.path.insert(1, 'ScriptsAndData')
 
 ##nice spreadsheet with prices: https://docs.google.com/spreadsheets/d/1_ej-xLzpVEvrGmp3JOXRFC5B_gHwJPMpB3SYMC3dDDY/edit#gid=0
 
+
+
+
+import requests
+import json
+import string
+import sys
+sys.path.insert(1, 'ScriptsAndData')
+
+BestProduct = "none"
+HighestProfit = ("0")
+HighestProfit = float(HighestProfit)
+#print(type(HighestProfit))
+
     #Getting the ApiKey (Your ApiKey will not be sended anywhere! Read the code!)
 with open('ApiKeyInHere.txt', 'r') as KeyTXT:
   ApiKey = KeyTXT.read()
@@ -20,22 +27,6 @@ with open('ApiKeyInHere.txt', 'r') as KeyTXT:
 
 Profit = "0"
        
-
-
-
-    #Product you wanna see?
-print()
-print()
-#Product = input("Product you want to see? For example: Wheat\n")
-#print("Ok, you want to see the product " + Product)
-#ApiKey = input("Your ApiKey?\n")
-#print("Ok, your ApiKey is:  " + ApiKey)
-#Product = Product.lower()
-#NormalPName = Product
-    #making input lowercase 
-#Product = Product.lower()
-    #testing something
-
 
     # read file
 with open('Prices.json', 'r') as prices:
@@ -50,16 +41,7 @@ for x in (NPCPrices["productIds"]):
     #making input lowercase 
     Product = x
     NormalPName = Product
-   #MERSHANT PRICES
-
-    # read file
-#with open('Prices.json', 'r') as prices:
-#    data=prices.read()
-
-    # parse file
-#NPCPrices = json.loads(data)
-#print("succses?: " + str(obj['succses']))
-
+    
     FileReadSuccses = (NPCPrices["success"])
     #print("Was reading the file a succses? " + FileReadSuccses)
 
@@ -90,11 +72,6 @@ for x in (NPCPrices["productIds"]):
     result = str(JSONData)
 
 
-    #did it sucsess?
-    #print("Was connecting to the Hypixel API a succses?")
-    #print(JSONData["success"])
-
-
     #get the prices
     sellPrice = (JSONData["product_info"]['quick_status']['buyPrice'])
     buyPrice = (JSONData['product_info']['quick_status']['sellPrice'])
@@ -105,11 +82,14 @@ for x in (NPCPrices["productIds"]):
     rBuyPrice = round(buyPrice, 2)
 
     #make the prices to floats
-    FrSellPrice = str(rSellPrice)  # float -> str
-    FrBuyPrice = str(rBuyPrice)  # float -> str
+    frSellPrice = str(rSellPrice)  # float -> str
+    frBuyPrice = str(rBuyPrice)  # float -> str
+
+    #FrSellPrice = float(rSellPrice)
+    #FrBuyPrice = float(rBuyPrice)
 
     #print the prices
-    print("You can buy " + NormalPName + " from the bazaar for " + FrBuyPrice + "$ and sell it to the bazaar for " + FrSellPrice + "$")
+    #print("You can buy " + NormalPName + " from the bazaar for " + frBuyPrice + "$ and sell it to the bazaar for " + frSellPrice + "$")
 
 
     #Do you make Profit?
@@ -119,29 +99,32 @@ for x in (NPCPrices["productIds"]):
 #Profit = float(Profit)
     rProfit = round(Profit, 2)
     srProfit = str(rProfit)
-
-    print()
-    print()
+    frProfit = float(rProfit)
     print()
     print("--------------------------------------")
 
 
     #if FrSellPrice > fNPCBuyPrice:
     if srProfit > "0":
-        print("You are making " + srProfit + "$ with this, do it!")
-        print()
-        print()
-        print()
-    else:
-        print("You are losing money, ("+ srProfit + "$) dont do it!")
-        print()
-        print()
-        print()
+         print("You are making " + srProfit + "$ with this, do it!")
+#        print()
+#    else:
+         print("You are losing money, ("+ srProfit + "$) dont do it!")
+#        print()
 
-    
-        #Print every avaiable Product, remove #
-#for x in NPCPrices["productIds"]: print(x)
-print()
+    if frProfit > HighestProfit:
+        print()
+        print("Highest profit margin so far")
+        print()
+        HighestProfit = frProfit
+        BestProduct = NormalPName
+        
+#    else:
+#        print()
+        print("Not the highest profit margin so far")
+#        print()
+strHighestProfit = str(HighestProfit)
+print("the highest profit margin is: " + strHighestProfit + "$ with the product " + BestProduct)
 print()
 print()
 print("Buy me VIP, IGN: flimmerkraft")
